6ae1df6f66100f5b41edc734889d5f0c3c72e33d
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2026-06-10 02:24:55 +0300

    add Salary History and @ManyToOne with Employee.

--- src/main/java/com/abdullah/entity/Employee.java
+++ src/main/java/com/abdullah/entity/Employee.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "employees")
@@ -50,9 +51,35 @@ public class Employee {
     )
     private EmployeeProfile employeeProfile;
 
+    @OneToMany(
+            mappedBy = "employee",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            orphanRemoval = true
+    )
+    @OrderBy("changeDate DESC")
+    private List<SalaryHistory> salaryHistory;
+
     public void setProfile(EmployeeProfile employeeProfile) {
         this.employeeProfile = employeeProfile;
         employeeProfile.setEmployee(this);
 
     }
+     public void addSalaryHistory(SalaryHistory salaryHistory) {
+        this.salaryHistory.add(salaryHistory);
+        salaryHistory.setEmployee(this);
+     }
+
+     public void removeSalaryHistory(SalaryHistory salaryHistory) {
+        this.salaryHistory.remove(salaryHistory);
+        salaryHistory.setEmployee(null);
+     }
+
+//    setSalaryHistory(List<SalaryHistory> salaryHistory) {
+//        this.salaryHistory = salaryHistory;
+//        for (SalaryHistory salaryHistoryItem : salaryHistory) {
+//            salaryHistoryItem.setEmployee(this);
+//        }
+//    }
+
 }
---
+++ src/main/java/com/abdullah/entity/SalaryHistory.java
@@ -0,0 +1,45 @@
+package com.abdullah.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "salary_history")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SalaryHistory {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,
+            precision = 15,
+            scale = 2)
+    private BigDecimal oldSalary;
+
+    @Column(nullable = false,
+            precision = 15,
+            scale = 2)
+    private BigDecimal newSalary;
+
+    @Column(nullable = false)
+    private LocalDate changeDate;
+
+    @Column(length = 500)
+    private String reason;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "employee_id",
+            nullable = false
+    )
+    private Employee employee;
+
+}
